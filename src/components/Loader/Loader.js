d836a19f25629d2c3c7b543f7c3f6f379d2faba3
Samira <samirasaad@gmail.com>
2020-11-24 21:29:31 +0200

    convert loader to functional component

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { css } from "@emotion/core";
 import PulseLoader from "react-spinners/PulseLoader";
-// import './Loader.scss'
 
 // Can be a string as well. Need to ensure each key-value pair ends with ;
 const styles = css`
@@ -11,27 +10,17 @@ const styles = css`
   right: 50%;
 `;
 
-class Loader extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: false,
-    };
-  }
-
-  render() {
-    const { loading } = this.props;
-    return (
-      <div className="loader-icon">
-        <PulseLoader
-          className="test"
-          css={styles}
-          size={30}
-          color={"#fccbff"}
-          loading={loading}
-        />
-      </div>
-    );
-  }
-}
+const Loader = ({ loading }) => {
+  return (
+    <div className="loader-icon">
+      <PulseLoader
+        className="test"
+        css={styles}
+        size={30}
+        color={"#fccbff"}
+        loading={loading}
+      />
+    </div>
+  );
+};
 export default Loader;
